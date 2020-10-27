0532ca0e737778ad173a95f431047a90b0911917
wudibin <781721794@qq.com>
2020-10-27 19:54:37 +0800

    [wu] font : add Group Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Group from './components/Group';
 import './App.scss';
 
 class App extends Component {
   render() {
-    return <div data-testid="app" className="App" />;
+    return (
+      <div data-testid="app" className="App">
+        <Group />
+      </div>
+    );
   }
 }
 
---
+++ src/App/components/Group.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Group extends Component {
+  render() {
+    return (
+      <div className="group">
+        <h3>分组列表</h3>
+      </div>
+    );
+  }
+}
+
+export default Group;
