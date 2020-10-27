52e7a9dd857ecd65522e4f448d99d2915660ba7b
wudibin <781721794@qq.com>
2020-10-27 21:35:42 +0800

    [wu] font : fetch students data & display

--- src/App/components/Student.js
+++ src/App/components/Student.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import '../styles/Student.css';
 
+const URL = 'http://localhost:8080/students';
+
 class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({
+          data: result.students,
+        });
+      });
+  }
+
   render() {
     return (
       <div className="student">
         <div className="studentTitle">学员列表</div>
         <div className="studentList">
-          <div className="studentMember">1.成吉思汗</div>
-          <div className="studentMember">2.成吉思汗</div>
-          <div className="studentMember">3.成吉思汗</div>
+          {Object.keys(this.state.data).map((key) => (
+            <div key={key}>
+              <div className="studentMember">
+                {this.state.data[key].id}.{this.state.data[key].name}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     );
