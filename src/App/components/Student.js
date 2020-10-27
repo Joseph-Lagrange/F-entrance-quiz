cf3f3f327a2e39cfbb136cd67a76241bf7d3f7f5
wudibin <781721794@qq.com>
2020-10-27 21:20:39 +0800

    [wu] font : add student components & add student list css

--- src/App/components/Student.js
+++ src/App/components/Student.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import '../styles/Student.css';
 
 class Student extends Component {
   render() {
-    return <h1>Student</h1>;
+    return (
+      <div className="student">
+        <div className="studentTitle">学员列表</div>
+        <div className="studentList">
+          <div className="studentMember">1.成吉思汗</div>
+          <div className="studentMember">2.成吉思汗</div>
+          <div className="studentMember">3.成吉思汗</div>
+        </div>
+      </div>
+    );
   }
 }
 
