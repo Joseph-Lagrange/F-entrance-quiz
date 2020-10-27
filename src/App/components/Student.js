2cc9381b9acad3551935e1a71dcd18cae56becba
wudibin <781721794@qq.com>
2020-10-27 22:27:26 +0800

    [wu] font : fetch add student data & add student list css

--- src/App/components/Student.js
+++ src/App/components/Student.js
@@ -4,10 +4,22 @@ import '../styles/Student.css';
 const URL = 'http://localhost:8080/students';
 
 class Student extends Component {
+  static checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    }
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       data: {},
+      student: {
+        name: '',
+      },
     };
   }
 
@@ -21,6 +33,35 @@ class Student extends Component {
       });
   }
 
+  handleChange = (event) => {
+    this.setState({
+      student: {
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8080/student', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state.student.name),
+    })
+      .then(Student.checkStatus)
+      .then(() => {
+        fetch(URL)
+          .then((response) => response.json())
+          .then((result) => {
+            this.setState({
+              data: result.students,
+            });
+          });
+      });
+  };
+
   render() {
     return (
       <div className="student">
@@ -33,6 +74,17 @@ class Student extends Component {
               </div>
             </div>
           ))}
+          <form className="form-box" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              className="studentMember"
+              onChange={this.handleChange}
+              value={this.state.student.name}
+              placeholder="      + 添加学员"
+              style={{ backgroundColor: '#ffffff', height: '31px' }}
+            />
+          </form>
         </div>
       </div>
     );
